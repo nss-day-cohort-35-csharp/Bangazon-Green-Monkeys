171246d66d32e9f29fca5c31671465984dd77ace
DylPickle11 <Dylan.Griff11@gmail.com>
2019-12-19 13:32:11 -0600

    All of the models

---
+++ BangazonAPI/Models/UserPaymentType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class UserPaymentType
+    {
+        public int Id { get; set; }
+        public string AccountNumber { get; set; }
+        public bool Active { get; set; }
+        public int CustomerId { get; set; }
+        public int PaymentId { get; set; }
+
+    }
+}
