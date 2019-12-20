9c910812fba4f186f815ca9ebb50dc4285dbf278
DylPickle11 <Dylan.Griff11@gmail.com>
2019-12-20 11:02:34 -0600

    fixed product

--- BangazonAPI/Models/Product.cs
+++ BangazonAPI/Models/Product.cs
@@ -8,30 +8,12 @@ namespace BangazonAPI.Models
     public class Product
     {
         public int Id { get; set; }
-<<<<<<< HEAD
         public int ProductTypeId { get; set; }
         public int CustomerId { get; set; }
         public decimal Price { get; set; }
         public string Description { get; set; }
         public string Title { get; set; }
         public DateTime DateAdded { get; set; }
-        
        
-        
-        
-        
-      
-        
-
-=======
-
-        public DateTime DateAdded { get; set; }
-        public decimal Price { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public int ProductTypeId { get; set; }
-        public int CustomerId { get; set; }
-     
->>>>>>> master
     }
 }
