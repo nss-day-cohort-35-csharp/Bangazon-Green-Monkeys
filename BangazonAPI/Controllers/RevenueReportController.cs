eb2cc6276d246d11bfec3b392f8b31b8c43bd7ef
Shirish Shrestha <shrestha_sl@hotmail.com>
2020-01-07 09:29:59 -0600

    Revenue Report

---
+++ BangazonAPI/Controllers/RevenueReportController.cs
@@ -0,0 +1,71 @@
+﻿using BangazonAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RevenueReportController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public RevenueReportController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+       
+        [HttpGet]
+        public async Task<IActionResult> GetRevenueReport()
+        {
+
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+
+                    cmd.CommandText = @"SELECT pt.Id AS ProductTypeId, pt.[Name] AS ProductType, IsNULL((SELECT COUNT(OrderProduct.Id)*Product.Price 
+                                         FROM OrderProduct LEFT JOIN Product ON Orderproduct.ProductId = Product.Id
+                                         WHERE Product.ProductTypeId = pt.Id GROUP BY Product.Price),0) AS TotalRevenue
+                                         FROM ProductType pt";
+
+                    SqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+                    List<ProductTypeRevenue> productTypeRevenues = new List<ProductTypeRevenue>();
+
+                    while (reader.Read())
+                    {
+                        ProductTypeRevenue productTypeRevenue = new ProductTypeRevenue()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("ProductTypeId")),
+                            ProductTypeName = reader.GetString(reader.GetOrdinal("ProductType")),
+                            TotalRevenue = reader.GetDecimal(reader.GetOrdinal("TotalRevenue")),
+
+                        };
+
+                        productTypeRevenues.Add(productTypeRevenue);
+                    }
+
+                    reader.Close();
+                    return Ok(productTypeRevenues);
+                }
+            }
+        }
+
+    }
+}
